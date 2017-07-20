ba30be3d4e9330aa77b3fea5fb5f6e9a2a63ae87
Stephane David <stephane.david@1-gate.com.com>
2017-07-20 17:24:42 +0200

    new file:   install_server.sh

---
+++ install_server.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Stop on the first sign of trouble
+set -e
+
+if [ $UID != 0 ]; then
+    echo "ERROR: Operation not permitted. Forgot sudo?"
+    exit 1
+fi
+
+echo "Install LoRaWAN Server"
+
+git clone http://github.com/1gate/1G-LWS-COM-868-XXX
+cd 1G-LWS-COM-868-XXX
+./update_hostname.sh
+cd 1G-LWS-COM-868-XXX/semtech
+./install_semtech.sh
+cd 1G-LWS-COM-868-XXX/gotthardp
+./install_lws.sh
+history -c
+history -w
+./tools/rfs_ro.sh
+
+
+echo "The system will reboot in 5 seconds..."
+sleep 5
+shutdown -r now
