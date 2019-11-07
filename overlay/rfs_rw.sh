aa4fd11fb07df5857ae411d3e59f56eb906a56ab
Stéphane David <stephane.david@1-gate.com>
2019-11-07 16:34:56 +0100

    Update rfs_rw.sh

--- overlay/rfs_rw.sh
+++ overlay/rfs_rw.sh
@@ -10,8 +10,7 @@ fi
 
 echo "Set Root File System in Read Write Mode"
 
-umount /boot
-mount /dev/mmcblk0p1 /boot
+mount -o remount,rw /boot
 sed -i "s,init=/sbin/overlayRoot.sh,init=/sbin/init," /boot/cmdline.txt
 
 #echo "The system will reboot in 5 seconds..."
